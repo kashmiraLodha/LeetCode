da6c0a5b04be27625aba9a508cdbc9860fff1de0
Kashmira Lodha <kashmira12092001@gmail.com>
2022-02-17 10:38:04 +0530

    Time: 3 ms (89.64%), Space: 45.1 MB (19.42%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -1,11 +1,11 @@
 class Solution {
     List<List<Integer>> ans=new ArrayList<>();
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-       List<Integer> temp=new ArrayList<>();
+        List<Integer> temp=new ArrayList<>();
         helper(candidates,target,temp,0);
         return ans;
     }
-    public void helper(int[] candidates,int target,List<Integer> temp,int index)
+    public void helper(int[]candidates,int target,List<Integer> temp,int index)
     {
         if(target==0)
         {
@@ -14,8 +14,9 @@ class Solution {
         }
         for(int i=index;i<candidates.length;i++)
         {
-            if(target>=candidates[i])
-            {   temp.add(candidates[i]);
+            if(candidates[i]<=target)
+            {
+                temp.add(candidates[i]);
                 helper(candidates,target-candidates[i],temp,i);
                 temp.remove(temp.size()-1);
             }
