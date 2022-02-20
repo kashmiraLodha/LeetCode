ece29cb829d84f8945b0042d26210f62d81747e4
Kashmira Lodha <kashmira12092001@gmail.com>
2022-02-20 14:20:21 +0530

    Time: 15 ms (90.00%), Space: 116.8 MB (60.00%) - LeetHub

--- 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
+++ 2178-maximum-split-of-positive-even-integers/2178-maximum-split-of-positive-even-integers.java
@@ -1,33 +1,24 @@
 class Solution {
-    Set<Long> ans;
-    
     public List<Long> maximumEvenSplit(long finalSum) {
-        if (finalSum % 2 != 0) {
-            return new ArrayList<>();
+        List<Long> res = new ArrayList<Long>();
+        //odd sum cannot be divided into even numbers
+        if(finalSum % 2 != 0) {
+            return res;
         }
-        
-        Set<Long> nums = new HashSet<>();
-        ans = new HashSet<Long>();
-        backtrackAndSum(finalSum, 0, nums);
-        
-        return new ArrayList<Long>(nums);
-    }
-    
-    private void backtrackAndSum(long finalSum, long prev, Set<Long> nums) {
-        if (finalSum == 0) {
-            ans = nums;
-            return;
+        //Greedy approach, try to build the total sum using minimum unique even nos
+        long currNum = 2;
+        long remainingSum = finalSum;
+        //as long as we can add subtract this number from remaining sum
+        while(currNum <= remainingSum) {
+            res.add(currNum);
+            remainingSum -= currNum;//reducing remaining sum
+            currNum += 2;//next even number
         }
-
-        for(long num = prev + 2; num <= finalSum; num += 2) {
-            nums.add(num);
-            backtrackAndSum(finalSum - num, num, nums);
-            
-            if (ans.size() > 0) {
-                break;
-            }
-            
-            nums.remove(num);
-        }
+        //now, remaining sum cannot be fulfilled by any larger even number
+        //so extract the largest even number we added to the last index of res, and make it even larger by adding this current remaining sum
+        //add remaining sum to the last element
+        long last = res.remove(res.size()-1);
+        res.add(last+remainingSum);
+        return res;
     }
 }
