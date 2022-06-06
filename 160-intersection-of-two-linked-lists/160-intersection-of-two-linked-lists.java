34232bd7afb7a57d3f553defc771780d6d08de43
Kashmira Lodha <kashmira12092001@gmail.com>
2022-06-06 08:10:45 +0530

    Time: 1 ms (99.17%), Space: 54.5 MB (65.01%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,18 +11,63 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-       ListNode a=headA,b=headB;
-       Set<ListNode> seen  = new HashSet<>();
-       while(a!=null){
-           seen.add(a);
-           a=a.next;
-       }
-    
-       while(b!=null){
-           if(!seen.add(b)) 
-               return b;
-           b=b.next;
-       }
-       return null;
+        ListNode A=headA;
+        ListNode B=headB;
+        int count=0;
+        while(A!=null && B!=null)
+        {
+            A=A.next;
+            B=B.next;
+            count++;
+        }
+        int temp=count;
+        boolean flag=false;
+        if(A!=null)
+        {   flag=true;
+            while(A!=null)
+            {
+                A=A.next;
+                count++;
+            }
+        }
+        boolean flag1=false;
+        if(B!=null)
+        {
+            flag1=false;
+            while(B!=null)
+            {
+                B=B.next;
+                count++;
+            }
+        }
+        A=headA;
+        B=headB;
+        if(flag==true)
+        {
+            for(int i=0;i<count-temp;i++)
+            {
+                A=A.next;
+            }
+        }
+        else
+        {
+            for(int i=0;i<count-temp;i++)
+            {
+                B=B.next;
+            }
+        }
+        while(A!=null&&B!=null)
+        {
+            if(A==B)
+            {
+                return A;
+                
+            }
+            A=A.next;
+            B=B.next;
+        }
+        return null;
+        
+        
     }
 }
