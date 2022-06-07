44492054ce90d15b81c9c4f94057afceea4a1f36
Kashmira Lodha <kashmira12092001@gmail.com>
2022-06-07 09:21:05 +0530

    Time: 1 ms (99.99%), Space: 56.7 MB (18.64%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,6 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        return nums[nums.length/2];
+        int count=0;
+        int ans=0;
+        
+       for(int i=0;i<nums.length;i++)
+       {
+           if(count==0)
+           {
+               ans = nums[i];
+               count++;
+           }
+           else if(nums[i]!=ans)
+           {
+               count--;
+           }
+           else
+               count++;
+       }
+        return ans;
     }
 }
