c8e404c3a86bdcce6bb670369c1404c3ba5082a7
Kashmira Lodha <kashmira12092001@gmail.com>
2022-09-15 13:50:21 +0530

    Time: 5 ms (10.82%), Space: 65.4 MB (5.06%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void rotate(int[] arr, int k) {
+        int n = arr.length;
+        if (n==1){
+            return;
+        }
+        k=k%n;
+        int i = n-k;
+        int j = n-1;
+        while(i<j){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+        int r = n-k-1;
+        int s = 0;
+        while(s<r){
+            int temp = arr[r];
+            arr[r] = arr[s];
+            arr[s] = temp;
+            r--;
+            s++;
+        }
+        int p = 0;
+        int q = n-1;
+        while(p<q){
+            int temp = arr[p];
+            arr[p] = arr[q];
+            arr[q] = temp;
+            p++;
+            q--;
+        }
+         
+    }
+}
